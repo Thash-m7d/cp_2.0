4332bb293f44bda66563e062a64ee26155a7c4ac
Thash-m7d <55905271+Thash-m7d@users.noreply.github.com>
2020-06-06 16:48:43 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,18 @@
+//To find factorial
+#include<stdio.h>
+int factorial(int n)
+  {
+      int i,fact=1;
+      for(i=1;i<=n;i++)
+      {
+        fact*=i;
+      }
+      return fact;
+  }
+void main()
+  {
+      int n;
+      printf("Enter a number : ");
+      scanf("%d",&n);
+      printf("\nThe factorial of %d = %d",n,factorial(n));
+  }
